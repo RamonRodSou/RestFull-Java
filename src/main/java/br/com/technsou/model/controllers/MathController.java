d073b99f8598de08249560ebb8b395f8b0b62df0
Ramon <ramonrodsouza@gmail.com>
2025-07-26 13:27:54 -0300

    feat: 003 - Criando Math e usando validacoes como IsNumeric e ConvertToDouble para validar uma string e converter para numero

---
+++ src/main/java/br/com/technsou/model/controllers/MathController.java
@@ -0,0 +1,32 @@
+package br.com.technsou.model.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    public static Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0d;
+        String number = strNumber.replaceAll(",", ".");// Moeda Americana x Brasileira
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+}
