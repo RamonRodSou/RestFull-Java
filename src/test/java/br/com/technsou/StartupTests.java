926ea49113f292e0e7c71e18aa35051dc423df2b
Ramon <ramonrodsouza@gmail.com>
2025-07-26 12:37:40 -0300

    feat: 001 - Criando um Api RestFull

---
+++ src/test/java/br/com/technsou/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.technsou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
