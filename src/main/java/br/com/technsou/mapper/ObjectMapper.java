b83317295cf9ab58dbde3def709d7fe5bf6c794b
Ramon <ramonrodsouza@gmail.com>
2025-08-02 10:43:29 -0300

    feat: 18 - feat: Criando ObjectMapper

---
+++ src/main/java/br/com/technsou/mapper/ObjectMapper.java
@@ -0,0 +1,26 @@
+package br.com.technsou.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectMapper {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <Origin, Destination> Destination parseObject(Origin origin, Class<Destination> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <Origin, Destination> List<Destination> parseListObjects(List<Origin> origin, Class<Destination> destination) {
+        List<Destination> destinationObjects = new ArrayList<>();
+        for (Origin o : origin) {
+            destinationObjects.add(mapper.map(o, destination));
+        }
+
+        return destinationObjects;
+    }
+
+}
